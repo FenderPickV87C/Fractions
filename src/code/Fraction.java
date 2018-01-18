cf219569652a12a8a11727fea1426175624c4b02
FenderPickV87C <jacob.wynn199@gmail.com>
2018-01-18 09:20:33 -0600

    Added the add method.

--- src/code/Fraction.java
+++ src/code/Fraction.java
@@ -2,7 +2,10 @@ package code;
 
 public class Fraction
 {
-    public Fration(int num, int denom)
+    private int num;
+    private int denom;
+
+    public Fraction(int num, int denom)
     {
         if (denom == 0)
         {
@@ -24,10 +27,37 @@ public class Fraction
         return 1;
     }
 
-    private void simplify()
-    {
 
-        int gcf(num, denom);
+    public String toString()
+    {
+        return num + " / " + denom;
+    }
+
+    public Fraction multiply (Fraction f)
+    {
+        return new Fraction(num*f.num, denom*f.denom);
+    }
+    public Fraction divide (Fraction f)
+    {
+        return new Fraction(num*f.denom, denom*f.num);
+    }
+
+
+    public static int lcm (int a, int b)
+    {
+        int mult = Math.max(a, b);
+
+        while (mult % a != 0 && mult % b != 0)
+        {
+            mult += Math.max(a, b);
+        }
+
+        return mult;
+    }
+
+    private void simplify ()
+    {
+        int factor = gcf (num, denom);
         num /= factor;
         denom /= factor;
     }
--- src/code/Runner.java
+++ src/code/Runner.java
@@ -4,7 +4,7 @@ public class Runner
 {
     public static void main(String[] args)
     {
-        /*Fraction f1 = null;
+        Fraction f1 = null;
         try
         {
             f1 = new Fraction(3, 0);
@@ -14,9 +14,11 @@ public class Runner
             System.out.println("Fractions need a non-zero denominator");
         }
         System.out.println(f1);
-        */
 
-        Fraction myFraction = new Fraction (12, 4);
+
+        Fraction myFraction = new Fraction(12, 4);
         System.out.println(myFraction);
+
+        System.out.println(Fraction.lcm(7, 5));
     }
 }
